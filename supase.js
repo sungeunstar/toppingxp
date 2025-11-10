6b65ba95cb67dacdabeef8c90ca420a6b2bd8f5c
ralph <sltime99@gmail.com>
2025-11-10 15:36:17 +0900

    auto deploy: 2025-11-10 15:36:17.16

---
+++ supase.js
@@ -0,0 +1,5 @@
+// js/supabase.js
+const SUPABASE_URL = "https://mgpxqmqvlbjfywbbsiwt.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ncHhxbXF2bGJqZnl3YmJzaXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NDc3OTYsImV4cCI6MjA3ODMyMzc5Nn0.dMKOkI0Vsty6ZMOxJmfF4IAPmkKF4kPhRhjCLH0dxEk";
+
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
