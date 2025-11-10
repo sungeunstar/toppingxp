3a3dc46133f616775bd93687dd9e334cfaa07a3d
ralph <sltime99@gmail.com>
2025-11-10 13:51:36 +0900

    auto deploy: 2025-11-10 13:51:36.26

---
+++ env.js
@@ -0,0 +1,6 @@
+<script>
+  window.__SUPABASE__ = {
+    url: "https://mgpxqmqvlbjfywbbsiwt.supabase.co",        // 대시보드 값으로 교체
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ncHhxbXF2bGJqZnl3YmJzaXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NDc3OTYsImV4cCI6MjA3ODMyMzc5Nn0.dMKOkI0Vsty6ZMOxJmfF4IAPmkKF4kPhRhjCLH0dxEk"                 // 대시보드 값으로 교체
+  };
+</script>
